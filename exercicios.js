f16accb050547566b6884373a70b5050583b4703
Elthon <eltonsilva.mecatronic@gmail.com>
2023-08-02 22:04:04 -0300

    Exercicio 03

--- exercicios.js
+++ exercicios.js
@@ -8,6 +8,11 @@ Na classe "Pessoa" criada anteriormente, adicione um método chamado "apresentar
  que exiba no console uma mensagem com o nome e a idade da pessoa.
  */
 
+/**
+Crie uma classe chamada "Aluno" que herde da classe "Pessoa" (do exercício 1).
+A classe "Aluno" deve ter um atributo adicional chamado "matricula".
+Crie um objeto chamado "aluno1" que seja um aluno e imprima no console.
+ */ 
 
 class Pessoa {
     constructor(nome, idade) {
@@ -21,3 +26,13 @@ class Pessoa {
 
 const pessoa1 = new Pessoa("Elthon", 27);
 pessoa1.apresentar();
+
+class Aluno extends Pessoa {
+   constructor(nome, idade, matricula) {
+      super(nome, idade) //super serve especificamente para herdar atributos da classe pai
+       this.matricula = matricula;
+  }
+}
+
+const aluno1 = new Aluno("Elthon", 27, 1);
+console.log(aluno1);
