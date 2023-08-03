365bafc62de76fef2cce23a6ec41d3326383de80
Elthon <eltonsilva.mecatronic@gmail.com>
2023-08-02 21:09:59 -0300

    Exercicio 02

--- exercicio01.js
+++ exercicio01.js
@@ -3,12 +3,21 @@ Crie uma classe chamada "Pessoa" que tenha os atributos "nome" e "idade".
 Em seguida, crie um objeto chamado "pessoa1" e atribua a ele um nome e uma idade.
  */
 
+/**
+Na classe "Pessoa" criada anteriormente, adicione um método chamado "apresentar"
+ que exiba no console uma mensagem com o nome e a idade da pessoa.
+ */
+
+
 class Pessoa {
     constructor(nome, idade) {
         this.nome = nome;
         this.idade = idade;
     }
+    apresentar() {
+        console.log("Nome: " + this.nome, "idade: " + this.idade);
+    }
 }
 
 const pessoa1 = new Pessoa("Elthon", 27);
-console.log(pessoa1);
+pessoa1.apresentar();
